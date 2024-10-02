c8ec6c0e4311bcd84c74a06c1fe63f2431754a5d
Cyrus Irani <cri23@cornell.edu>
2024-10-01 21:07:00 -0400

    fixed firebaseConfig

--- public/app.js
+++ public/app.js
@@ -16,14 +16,15 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "REPLACE",
+  authDomain: "syntara-88cc3.firebaseapp.com",
+  projectId: "syntara-88cc3",
+  storageBucket: "syntara-88cc3.appspot.com",
+  messagingSenderId: "311542379658",
+  appId: "1:311542379658:web:166dc3fd29797b9152a973",
+  measurementId: "G-EMH5ZCYG9W"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
