f412527de3834f5d6d2a0b501c72df5e436f6ed8
Cyrus Irani <cri23@cornell.edu>
2024-10-01 20:49:27 -0400

    changed to .env file

--- public/app.js
+++ public/app.js
@@ -16,13 +16,13 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "REPLACE", // Replace with your actual API key
-  authDomain: "syntara-88cc3.firebaseapp.com",
-  projectId: "syntara-88cc3",
-  storageBucket: "syntara-88cc3.appspot.com",
-  messagingSenderId: "311542379658",
-  appId: "1:311542379658:web:166dc3fd29797b9152a973",
-  measurementId: "G-EMH5ZCYG9W",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
