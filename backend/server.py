d800a41d5c71ec490ce9bc34e49c6db5b3f33cf2
Ankit Lakkapragada <ankit.lakkapragada@gmail.com>
2024-10-27 14:56:00 -0400

    Adding server processing

--- backend/server.py
+++ backend/server.py
@@ -6,7 +6,7 @@ from sklearn.feature_selection import SelectKBest, f_classif
 from concrete.ml.sklearn import SGDClassifier
 from concrete.compiler import check_gpu_available
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.decomposition import PCA
 
 
@@ -16,7 +16,16 @@ class Server:
         self.models = {}
     
     def recieve_encrypted_data(self, condition, data):
-        x, y = data
+        x = data.iloc[:, :-1].copy()
+        y = data.iloc[:, -1].copy().squeeze()
+        
+        # Step 2: Encode categorical features in x
+        for column in x.select_dtypes(include=['object']).columns:
+            x[column] = LabelEncoder().fit_transform(x[column])
+        
+        # Step 3: Encode target variable y if categorical
+        if y.dtype == 'object':
+            y = LabelEncoder().fit_transform(y)
         x_scaled = MinMaxScaler(feature_range=[-1, 1]).fit_transform(x)
         #eventually make this check firebase
         if condition not in self.models:
