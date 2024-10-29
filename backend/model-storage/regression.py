275caa11a8fb6c520500a1a82510ba8b199f09b5
Rohan C. Shah <rcs353@cornell.edu>
2024-10-28 20:48:19 -0400

    dynamic module adding for models, as well as rename files

---
+++ backend/model-storage/regression.py
@@ -0,0 +1,5 @@
+# Sample regression model that is fed into save-model.py
+
+from sklearn.linear_model import LogisticRegression
+
+model = LogisticRegression(max_iter=1000)
--- backend/model-storage/save-model.py
+++ backend/model-storage/save-model.py
@@ -1,7 +1,9 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
+from regression import model
 import joblib
 import logging
+import importlib
 import os
 
 logging.basicConfig(level=logging.INFO)
@@ -10,20 +12,26 @@ MODELS_DIR = os.path.join(os.path.dirname(__file__), 'models')
 os.makedirs(MODELS_DIR, exist_ok=True)
 
 
-def train_and_save_model(filename: str):
-    filepath = os.path.join(MODELS_DIR, filename)
+def train_and_save_model(model_filename, saved_model_filename: str):
+    filepath = os.path.join(MODELS_DIR, saved_model_filename)
+
+    try:
+        module = importlib.import_module(model_filename)
+        model = getattr(module, 'model')
+    except (ModuleNotFoundError, AttributeError) as e:
+        logging.error(f"Error importing model: {e}")
+        return
 
     try:
         data = load_iris()
         X, y = data.data, data.target
-        model = LogisticRegression(max_iter=1000)
         model.fit(X, y)
 
         joblib.dump(model, filepath)
-        logging.info(f"Model trained and saved to {filename}")
+        logging.info(f"Model trained and saved to {saved_model_filename}")
     except Exception as e:
         logging.error(f"Error training or saving model: {e}")
 
 
 # Test with saving to 'logistic_model.joblib'
-train_and_save_model('logistic_model.joblib')
+train_and_save_model('regression', 'logistic_model.joblib')
