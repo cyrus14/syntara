bb2e6d0079a8e4ed49fa887fdab8472079ffef16
Rohan C. Shah <rcs353@cornell.edu>
2025-04-08 13:50:36 -0400

    temporarily add firebase config

---
+++ client/src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDG0vZ9p4X2XlIdP11RLIJ-FKMFvtYIbos",
+  authDomain: "syntara-88cc3.firebaseapp.com",
+  projectId: "syntara-88cc3",
+  storageBucket: "syntara-88cc3.appspot.com",
+  messagingSenderId: "311542379658",
+  appId: "1:311542379658:web:166dc3fd29797b9152a973",
+  measurementId: "G-EMH5ZCYG9W",
+};
+export default firebaseConfig;
